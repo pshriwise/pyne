a5ef3d5d9fa1bb3ca50b1f8580015a825b3a8ea4
Patrick Shriwise <pshriwise@gmail.com>
2023-09-11 15:22:22 -0500

    Adding name to PyNE's isnan implementation

--- src/utils.h
+++ src/utils.h
@@ -27,14 +27,14 @@
 
 #if (__GNUC__ >= 4)
   #include <cmath>
-  #define isnan(x) std::isnan(x)
+  #define _pyne_isnan(x) std::isnan(x)
 #else
   #include <math.h>
-  #define isnan(x) __isnand((double)x)
+  #define _pyne_isnan(x) __isnand((double)x)
 #endif
 
 #ifdef _WIN32
-#define isnan(x) ((x) != (x))
+#define _pyne_isnan(x) ((x) != (x))
 #endif
 
 #ifndef JSON_IS_AMALGAMATION
